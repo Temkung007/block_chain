9033891391f8d2d343405e961c81426980076843
Suraphong Philasri <87115844+Temkung007@users.noreply.github.com>
2022-01-14 14:07:45 +0700

    blockchain
    basic blockchain with python

--- blockchain.py
+++ blockchain.py
@@ -7,6 +7,8 @@ from flask import Flask, jsonify
 class Blockchain:
     def __init__(self):
         self.chain = []
+        self.transaction = 0
+        # genesis block
         self.create_block(nonce=1, previous_hash="0")
 
     def create_block(self, nonce, previous_hash):
@@ -14,6 +16,7 @@ class Blockchain:
             "index": len(self.chain)+1,
             "timestamp": str(datetime.datetime.now()),
             "nonce": nonce,
+            "data": self.transaction,
             "previous_hash": previous_hash
         }
         self.chain.append(block)
@@ -46,6 +49,17 @@ class Blockchain:
         block_index = 1
         while block_index < len(chain):
             block = chain[block_index]
+            if block["previous_hash"] != self.hash(previous_block):
+                return False
+            previous_nonce = previous_block["nonce"]
+            nonce = block["nonce"]
+            hashoperation = hashlib.sha256(str(nonce ** 2 - previous_nonce ** 2).encode()).hexdigest()
+            if hashoperation[:4] != "0000":
+                return False
+            previous_block = block
+            block_index += 1
+
+        return True
 
 
 blockchain = Blockchain()
@@ -70,6 +84,9 @@ def get_chain():
 
 @app.route('/mining', methods=["GET"])
 def mining_block():
+    amount = 1000
+    blockchain.transaction = blockchain.transaction + amount
+
     previous_block = blockchain.get_previous_block()
     previous_nonce = previous_block["nonce"]
 
@@ -80,12 +97,23 @@ def mining_block():
     block = blockchain.create_block(nonce, previous_hash)
 
     response = {
-        "message": "Mining Block success",
+        "message": "Mining Block Success",
         "index": block["index"],
         "timestamp": block["timestamp"],
         "nonce": block["nonce"],
+        "data": block["data"],
         "previous_hash": block["previous_hash"]
     }
+    return jsonify(response), 200
+
+
+@app.route('/is_valid', methods=["GET"])
+def is_valid():
+    status = blockchain.is_chain_valid(blockchain.chain)
+    if status:
+        response = {"message": "Blockchain Is Valid"}
+    else:
+        response = {"message": "Have Problem, Blockchain Is Not Valid"}
     return jsonify(response), 200
 
 
